9aec3606bace07b71ec5bd47a14b13755f56bd93
Stefan <Stefan30.v.d.Merwe@gmail.com>
2017-02-11 22:01:13 +0200

    Strart of assessment for outsurence

---
+++ Outsurance.Assessment.Domain/Person/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outsurance.Assessment.Domain.Person
+{
+    class Person
+    {
+    }
+}
